415c25c4e735b9993770c11c6709490dad981175
jaysonad <89358921+jaysonad@users.noreply.github.com>
2024-04-03 12:58:00 -0400

    Update main.js

--- Assignment 4/Assignment 4 P4/main.js
+++ Assignment 4/Assignment 4 P4/main.js
@@ -6,9 +6,6 @@ let count = 0;
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
@@ -17,7 +14,8 @@ const height = (canvas.height = window.innerHeight);
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  return num;
+};
 
 // function to generate random color
 
@@ -25,6 +23,16 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
+class Shape {
+
+    constructor(x, y, velX, velY) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+    }
+  
+  }
 //modeling the ball giving it colour size, and action
 class Ball {
     constructor(x, y, velX, velY, color, size) {
